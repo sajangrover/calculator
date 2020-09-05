6b7ce522d0995d8e97d4ba42cd07c6a7f91770f1
sajangrover <sajangrover379@gmail.com>
2020-09-05 17:57:23 +0530

    start adding javascript logic

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
 const calculate = () =>{
     console.log('hello')
 }
+
+const getValue = (value) => {
+    const input = document.getElementById('data');
+    if(value=='sqrt'){
+    }
+    else{
+        input.value = input.value+value
+    }
+}
